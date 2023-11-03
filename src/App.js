e41e72a42c9035311705f0571c37558231748f38
akshitbalodhicuvette <akshitbalodhicuvette@gmail.com>
2023-11-03 18:12:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import DesktopView from "./view/DesktopView/DesktopView";
+import MobileView from "./view/MobileView/MobileView";
+import NotesMobilePage from "./components/notesMobilePage/NotesMobilePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "./context/PocketContext";
+import usePocketContext from "./hooks/usePocketContext";
 
 function App() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const { selected, setSelected } = usePocketContext();
+
+  useEffect(() => {
+    setSelected(localStorage.getItem("selected") || "");
+    // eslint-disable-next-line
+  }, [selected]);
+  const checkScreenSize = () => {
+    setScreenSize(window.innerWidth);
+  };
+
+  window.addEventListener("resize", checkScreenSize);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <div className="App">
+        {screenSize > 500 ? (
+          <DesktopView />
+        ) : (
+          <Router>
+            <Routes>
+              <Route path="/" element={<MobileView />} />
+              <Route path="/notes" element={<NotesMobilePage />} />
+            </Routes>
+          </Router>
+        )}
+      </div>
+    </Provider>
   );
 }
 
